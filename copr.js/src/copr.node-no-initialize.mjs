1070673f0db44f2cd3a83f5f2459973767f6ee71
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2023-08-27 13:52:43 +0200

    add copr.node-no-initialize.mjs

---
+++ copr.js/src/copr.node-no-initialize.mjs
@@ -0,0 +1,21 @@
+import COPR from './copr.mjs'
+import http from 'http'
+import * as jmespath from  'jmespath'
+import scheme from 'copr.common/scheme.json' assert {type: 'json'}
+
+/** INITIALIZE **/
+
+COPR._scheme = scheme
+COPR._jmespath = jmespath
+COPR._jsonFromURL = async url => new Promise(resolve => {
+  http.get(url, res => {
+    if (res.statusCode != 200) return
+    let data = ''
+    res.on('data', chunk => { data += chunk })
+    res.on('end', () => resolve(JSON.parse(data)))
+  })
+})
+
+/** EXPORT **/
+
+export default COPR
--- copr.js/src/copr.node.mjs
+++ copr.js/src/copr.node.mjs
@@ -1,23 +1,8 @@
-import COPR from './copr.mjs'
-import http from 'http'
-import * as jmespath from  'jmespath'
-import scheme from 'copr.common/scheme.json' assert {type: 'json'}
+import COPR from './copr.node-no-initialize.mjs'
 
 /** INITIALIZE **/
 
-COPR._scheme = scheme
-COPR._jmespath = jmespath
-COPR._jsonFromURL = async url => new Promise(resolve => {
-  http.get(url, res => {
-    if (res.statusCode != 200) return
-    let data = ''
-    res.on('data', chunk => { data += chunk })
-    res.on('end', () => resolve(JSON.parse(data)))
-  })
-})
-
-if (process && process.browser) console.log('Warning: COPR could not be initialized.  Run `await COPR._initialize()` before using COPR.')
-else await COPR._initialize()
+await COPR._initialize()
 
 /** EXPORT **/
 
